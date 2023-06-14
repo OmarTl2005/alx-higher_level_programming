40dcf42f06ea7563abf25366ac4847ff37a919ab
Omar Taouil <hiba41145@gmail.com>
2023-06-14 21:08:34 +0100

    fixed

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dictionary = {'I': 1, 'V': 5, 'X': 10,
+             'L': 50, 'C': 100, 'D': 500}
+    sum = 0
+    i = 0
+    if (roman_string is None or roman_string == ""):
+        return 0
+    while (i < len(roman_string)):
+        o = 1
+        if roman_string[i] == 'I' and i + 1 < len(roman_string):
+            if roman_string[i + 1] == 'X':
+                sum += 9
+                i += 1
+                o = 0
+        elif roman_string[i] == 'I' and i + 1 < len(roman_string):
+            if roman_string[i + 1] == 'V':
+                sum += 4
+                i += 1
+                o = 0
+        elif roman_string[i] == 'X' and i + 1 < len(roman_string):
+            if roman_string[i + 1] == 'L':
+                sum += 40
+                i += 1
+                o = 0
+        elif roman_string[i] == 'X' and i + 1 < len(roman_string):
+            if roman_string[i + 1] == 'C':
+                sum += 90
+                i += 1
+                o = 0
+        elif roman_string[i] == 'C' and i + 1 < len(roman_string):
+            if roman_string[i + 1] == 'D':
+                sum += 400
+                i += 1
+                o = 0
+        if o:
+            sum += dictionary[roman_string[i]]
+        i += 1
+    return sum
