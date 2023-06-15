30a6dec053bf5db2617b5c853ef2eaa8f413c6fc
OmarTl2005 <hiba41145@gmail.com>
2023-06-15 23:03:00 +0100

    fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
-
+/**
+  *print_dlistint - prints all the elements of a dlistint_t list.
+  *@h: var
+  *Return: count
+  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *curr = h;
-    size_t size = 0;
+	size_t count = 0;
 
-    if (!curr)
-        return (0);
-    while (curr)
-    {
-        printf("%d\n", curr->n);
-        curr = curr->next;
-        size++;
-    }
-    return (size);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
